c0b947f54a04d4540f1ddc26e68cdacddf14d2ca
Lucas Saramento Brandão <lucassbbrandao123@gmail.com>
2025-10-29 10:07:22 -0300

    Update poke-api.js

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,6 +1,3 @@
-
-const pokeApi = {}
-
 function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
     pokemon.number = pokeDetail.id
@@ -12,15 +9,10 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     pokemon.types = types
     pokemon.type = type
 
-    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+    // Linha corrigida:
+    pokemon.photo = pokeDetail.sprites.other['official-artwork'].front_default
 
     return pokemon
-}
-
-pokeApi.getPokemonDetail = (pokemon) => {
-    return fetch(pokemon.url)
-        .then((response) => response.json())
-        .then(convertPokeApiDetailToPokemon)
 }
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
